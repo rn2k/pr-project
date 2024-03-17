b2ebf84d15e7e386fdfaf18a7a418d7074163a9f
Eren Temel <erentemell35@gmail.com>
2024-03-17 12:03:10 +0300

    outline has been completed

---
+++ src/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    // toggleButton adlı butona tıklandığında
+    $("#toggleButton").click(function(){
+        // alanın gösterilip gizlenmesi
+        $("#area").toggle();
+    });
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container:{
+      center:true
+    },
+    extend: {
+      fontFamily:{
+        open: ["Open Sans", "sans-serif"]
+      },
+      colors:{
+        "pr-purpleB": "#7469B6",
+        "pr-purpleL": "#E1AFD1",
+      },
+      spacing:{
+        "128":"32rem",
+      }
+    },
   },
   plugins: [],
 }
