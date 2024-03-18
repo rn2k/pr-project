7eb38c2223cdb5da38054aa8411c097c7bd8ee4d
Eren Temel <erentemell35@gmail.com>
2024-03-18 12:09:02 +0300

    added contact

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,9 @@ module.exports = {
         open: ["Open Sans", "sans-serif"]
       },
       colors:{
-        "pr-purpleB": "#7469B6",
-        "pr-purpleL": "#E1AFD1",
+        "pr-purpleB": "#7F56D9",
+        "pr-purpleL": "#F9F5FF",
+        "pr-grey":"#F2F4F7"
       },
       spacing:{
         "128":"32rem",
